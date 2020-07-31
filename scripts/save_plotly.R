d627f2ac9f60642bcfdcbf663f2db7e873707fd7
Cédric Mondy <cedric.mondy@gmail.com>
2020-07-31 18:56:14 +0200

    refactor save_plotly function

--- scripts/save_plotly.R
+++ scripts/save_plotly.R
@@ -26,55 +26,44 @@ save_plotly <- function(x, file, file_static = NULL, title, local = FALSE, libdi
             (function(x2) {
                 x2$dependencies <- x2$dependencies %>% 
                     purrr::map(
-                        function(dep_i) {
+                        .x = .,
+                        .f = function(dep_i) {
+                            use_plotly_cdn <- function(bundle, dep) {
+                                if (dep$name == glue::glue("plotly-{bundle}")) {
+                                    dep$src <- "https://cdn.plot.ly/"
+                                    
+                                    bundle <- ifelse(
+                                        test = bundle == "main",
+                                        yes  = "",
+                                        no   = glue::glue("-{bundle}")
+                                        )
+                                    
+                                    dep$script <- glue::glue(
+                                        "plotly{bundle}-{dep$version}.min.js"
+                                    ) %>% 
+                                        as.character()
+                                }
+                                dep
+                            }
+                            
+                            plotly_bundles <- c(
+                                "basic", "cartesian", "geo", "gl3d", "gl2d",
+                                "mapbox", "finance", "main"
+                                )
+                            
                             if (dep_i$name == "jquery")
                                 dep_i$src <- list(href = glue::glue("https://cdnjs.cloudflare.com/ajax/libs/jquery/{dep_i$version}"))
                             
-                            if (dep_i$name == "plotly-basic") {
-                                dep_i$src <-  list(href = "https://cdn.plot.ly/")
-                                dep_i$script <- glue::glue("plotly-basic-{dep_i$version}.min.js")
-                            }
+                            if (dep_i$name %in% glue::glue("plotly-{plotly_bundles}")) 
+                                purrr::map(
+                                    .x = plotly_bundles,
+                                    .f = use_plotly_cdn,
+                                    dep = dep_i
+                                    )
                             
-                            if (dep_i$name == "plotly-cartesian") {
-                                dep_i$src <-  list(href = "https://cdn.plot.ly/")
-                                dep_i$script <- glue::glue("plotly-cartesian-{dep_i$version}.min.js")
-                            }
-                            
-                            if (dep_i$name == "plotly-geo") {
-                                dep_i$src <-  list(href = "https://cdn.plot.ly/")
-                                dep_i$script <- glue::glue("plotly-geo-{dep_i$version}.min.js")
-                            }
-                            
-                            if (dep_i$name == "plotly-gl3d") {
-                                dep_i$src <-  list(href = "https://cdn.plot.ly/")
-                                dep_i$script <- glue::glue("plotly-gl3d-{dep_i$version}.min.js")
-                            }
-                            
-                            if (dep_i$name == "plotly-gl2d") {
-                                dep_i$src <-  list(href = "https://cdn.plot.ly/")
-                                dep_i$script <- glue::glue("plotly-gl2d-{dep_i$version}.min.js")
-                            }
-                            
-                            if (dep_i$name == "plotly-mapbox") {
-                                dep_i$src <-  list(href = "https://cdn.plot.ly/")
-                                dep_i$script <- glue::glue("plotly-mapbox-{dep_i$version}.min.js")
-                            }
-                            
-                            if (dep_i$name == "plotly-finance") {
-                                dep_i$src <-  list(href = "https://cdn.plot.ly/")
-                                dep_i$script <- glue::glue("plotly-finance-{dep_i$version}.min.js")
-                            }
-                            
-                            if (dep_i$name == "plotly-main") {
-                                dep_i$src <-  list(href = "https://cdn.plot.ly/")
-                                dep_i$script <- glue::glue("plotly-{dep_i$version}.min.js")
-                            }
-                            
-                            dep_i$script <- purrr::map(dep_i$script, as.character)
                             dep_i
-                        }
-                    )
-                
+                            }
+                        )
                 x2
             }) %>% 
             htmlwidgets::saveWidget(widget        = .,
@@ -89,4 +78,3 @@ save_plotly <- function(x, file, file_static = NULL, title, local = FALSE, libdi
     
     c(file, file_static)
 }
-
